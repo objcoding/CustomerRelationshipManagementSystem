7d99f9a9fa1914aedde4e0f97ab7e134052873a9
zchdjb <zchdjb@foxmail.com>
2017-05-29 02:59:57 +0800

    no message

---
+++ src/main/java/com/crm/controller/WebController.java
@@ -0,0 +1,28 @@
+package com.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zch on 2017/4/29.
+ */
+@Controller
+@RequestMapping("/crm")
+public class WebController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/logout")
+    public String logout() {
+        return "logout";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/main/resources/static/user/user.js
@@ -0,0 +1,3 @@
+/**
+ * Created by zch on 2017/4/29.
+ */
